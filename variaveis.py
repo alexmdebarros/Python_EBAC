d848a2514a16ed921b3b99ee160a72aab2bcd136
Alex Martins <alexmdebarros@gmail.com>
2023-08-28 18:09:15 -0400

    colocando tipos de formatação

--- variaveis.py
+++ variaveis.py
@@ -1,7 +1,13 @@
 #estudo sobre variáveis
+
+#definindo as variáveis
 nome = "Alex"
 sobrenome = "Martins"
 idade = 32
 cargo = "Analista de dados"
 
-print(f"O {nome} {sobrenome} tem {idade} anos e trabalha como {cargo}")
+#mostrando na tela de forma formatada com {}
+print(f"O {nome} {sobrenome} tem {idade} anos e trabalha como {cargo}.")
+
+#mostrando na tela de forma fatorada
+print("O", nome, sobrenome, "tem",  idade, "anos e trabalha como", cargo,".")
